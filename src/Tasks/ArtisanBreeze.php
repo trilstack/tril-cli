3d0183bc1e0926ac6ac4d2f5e14577c7ed5dc059
JeroenG <jgroenendijk@enrise.com>
2023-03-20 12:53:48 +0100

    Add typescript flag

--- src/Tasks/ArtisanBreeze.php
+++ src/Tasks/ArtisanBreeze.php
@@ -6,5 +6,5 @@ namespace TrilStack\Cli\Tasks;
 
 final class ArtisanBreeze extends SimpleCommandTask implements TaskInterface
 {
-    protected string $command = "php artisan breeze:install react --no-interaction";
+    protected string $command = "php artisan breeze:install react --typescript --no-interaction";
 }
